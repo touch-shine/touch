a0ff353dccc5208111468e14292eb91be0d89fd3
touch-shine <touchandshine73@gmail.com>
2025-07-24 21:12:22 +0500

    GTM New layout.js
    GTM New layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,13 +32,21 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <head>
-    <!-- Google Tag Manager -->
+  
+<!-- Google Tag Manager As Per Jack-->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M9Q2NNKJ');</script>
 <!-- End Google Tag Manager -->
+ {/* <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M9Q2NNKJ');</script>
+<!-- End Google Tag Manager -->*/}
         {/* You can also explicitly add it like this */}
         {/* <meta
           name="google-site-verification"
@@ -110,10 +118,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><MetadataProvider>
-        <!-- Google Tag Manager (noscript) -->
+<!-- Google Tag Manager As Per Jack (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9Q2NNKJ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<!-- End Google Tag Manager (noscript) -->       
+ {/*<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9Q2NNKJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) --> */}
           <Navbar />
           <Metadata />
           {children}
