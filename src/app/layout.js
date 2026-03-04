4e36bc532504173c8f208ef4e70728d2f4109e44
touch-shine <touchandshine73@gmail.com>
2026-03-04 05:30:13 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -90,16 +90,7 @@ export const metadata = {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><MetadataProvider>
-{/* Google Tag Manager (noscript) */}
-<noscript
-  dangerouslySetInnerHTML={{
-    __html: `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9Q2NNKJ"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    `,
-  }}
-/>
-{/* End Google Tag Manager (noscript) */}
+
 
           <Navbar />
           <Metadata />
